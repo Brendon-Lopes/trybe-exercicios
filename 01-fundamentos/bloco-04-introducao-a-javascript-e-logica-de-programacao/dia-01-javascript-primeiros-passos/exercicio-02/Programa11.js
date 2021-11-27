40814b8c512b1b326e52d28b9abfba045ba771f5
Brendon Lopes <brendon.lopes.21@gmail.com>
2021-11-26 22:13:56 -0300

    Concluido programa que calcula salario liquido deduzindo INSS e IR

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-02/Programa11.js
@@ -0,0 +1,49 @@
+const salario = 3000;
+let salarioSemInss;
+let salarioFinal;
+let inss;
+let ir;
+
+switch(true){
+    case salario <= 1556.94:
+        inss = salario * 0.08;
+        break;
+    case salario <= 2594.92:
+        inss = salario * 0.09;
+        break;
+    case salario <= 5189.82:
+        inss = salario * 0.11;
+        break;
+    case salario > 5189.82:
+        inss = 570.88;
+        break;
+}
+
+salarioSemInss = salario - inss;
+
+switch(true){
+    case salarioSemInss <= 1903.98:
+        salarioFinal = salarioSemInss;
+        console.log('Salário Líquido: ' + salarioFinal);
+        break;
+    case salarioSemInss <= 2826.65:
+        ir = (salarioSemInss * 0.075) - 142.80;
+        salarioFinal = salarioSemInss - ir;
+        console.log('Salário Líquido: ' + salarioFinal);
+        break;
+    case salarioSemInss <= 3751.05:
+        ir = (salarioSemInss * 0.15) - 354.80;
+        salarioFinal = salarioSemInss - ir;
+        console.log('Salário Líquido: ' + salarioFinal);
+        break;
+    case salarioSemInss <= 4664.68:
+        ir = (salarioSemInss * 0.225) - 636.13;
+        salarioFinal = salarioSemInss - ir;
+        console.log('Salário Líquido: ' + salarioFinal);
+        break;
+    case salarioSemInss > 4664.68:
+        ir = (salarioSemInss * 0.275) - 869.36;
+        salarioFinal = salarioSemInss - ir;
+        console.log('Salário Líquido: ' + salarioFinal);
+        break;
+}
