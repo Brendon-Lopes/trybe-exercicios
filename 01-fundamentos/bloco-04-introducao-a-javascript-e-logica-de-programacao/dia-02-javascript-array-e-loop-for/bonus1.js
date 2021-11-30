fca62562ddb49d5b4105bb98253308aff5ea0b4b
Brendon Lopes <brendon.lopes.21@gmail.com>
2021-11-29 21:28:11 -0300

    Exercicio bonus terminado

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/bonus1.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/bonus1.js
@@ -1,12 +1,21 @@
 let array = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let arrayCopy = array.slice();
+let arrayMultiplied = [];
 
-for(let i = 1; i < arrayCopy.length - 1; i += 1){
+for(let i = 1; i < arrayCopy.length; i += 1){
     for(let j = 0; j < i; j += 1){
         if(arrayCopy[i] < arrayCopy[j]){
             [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
         }
     }
 }
-console.log(array);
+
+for(let k = 0; k < arrayCopy.length; k += 1){
+    if(k + 1 < arrayCopy.length){
+        arrayMultiplied.push(arrayCopy[k]*arrayCopy[k+1]);
+    } else{
+        arrayMultiplied.push(arrayCopy[k]*2);
+    }
+}
 console.log(arrayCopy);
+console.log(arrayMultiplied);
