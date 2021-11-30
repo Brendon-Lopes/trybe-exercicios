f5790b952861216ccf8541eef7234d0c7e4e30ef
Brendon Lopes <brendon.lopes.21@gmail.com>
2021-11-30 18:49:47 -0300

    Exercicio 2 ajustado

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio2.js
@@ -1,11 +1,12 @@
 const n = 5;
 let linha = '';
 
-for(let i = 1; i <=n; i += 1){
-    if(n > 1){
+if(n > 1){
+    for(let i = 1; i <=n; i += 1){
         linha += '* ';
+        
     }
-}
-for(let i2 = 1; i2 <=n; i2+=1){
-    console.log(linha);
+    for(let i2 = 1; i2 <=n; i2+=1){
+        console.log(linha);
+    }
 }
