a3d2d1a82d1f74a69f5d6a197544d4ee9c7ac677
Brendon Lopes <brendon.lopes.21@gmail.com>
2021-12-06 18:20:09 -0300

    todos os exercicios de fixaçao finalizados!

--- 01-fundamentos/bloco-05-javascript-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicio-01/script.js
+++ 01-fundamentos/bloco-05-javascript-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicio-01/script.js
@@ -11,4 +11,17 @@ elementoOndeVoceEsta.appendChild(document.createElement('p'));
 
 primeiroFilhoDoFilho.appendChild(document.createElement('p'));
 
-console.log(document.getElementById('primeiroFilhoDoFilho').firstElementChild);
+console.log(document.getElementById('primeiroFilhoDoFilho').firstElementChild.parentElement.lastElementSibling);
+
+let paiDoPai = document.getElementById('paiDoPai');
+
+pai.removeChild(document.getElementById('primeiroFilho'));
+pai.removeChild(document.getElementById('terceiroFilho'));
+pai.removeChild(document.getElementById('quartoEUltimoFilho'));
+elementoOndeVoceEsta.removeChild(document.getElementById('segundoEUltimoFilhoDoFilho'));
+primeiroFilhoDoFilho.removeChild(document.getElementsByTagName('p')[0]);
+elementoOndeVoceEsta.removeChild(document.getElementsByTagName('p')[0]);
+pai.removeChild(document.querySelectorAll('section')[3]);
+
+// pai.removeChild(pai.getElementsByTagName('section')[2]);
+// também funciona! ^
