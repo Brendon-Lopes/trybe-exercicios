e19d240ff83c18caf162038671dc23d836268966
Brendon Lopes <brendon.lopes.21@gmail.com>
2021-12-06 15:58:38 -0300

    Continuacao do exercicio

--- 01-fundamentos/bloco-05-javascript-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicio-01/script.js
+++ 01-fundamentos/bloco-05-javascript-DOM-Eventos-e-Web-Storage/dia-2-javascript-trabalhando-com-elementos/exercicio-01/script.js
@@ -1,2 +1,14 @@
 document.getElementById('elementoOndeVoceEsta').parentElement.style.backgroundColor = 'green';
 document.getElementById('elementoOndeVoceEsta').firstElementChild.innerText = 'Esse é o primeiro filho do filho';
+
+let elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
+let pai = document.getElementById('pai');
+let primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
+
+pai.appendChild(document.createElement('section'));
+
+elementoOndeVoceEsta.appendChild(document.createElement('p'));
+
+primeiroFilhoDoFilho.appendChild(document.createElement('p'));
+
+console.log(document.getElementById('primeiroFilhoDoFilho').firstElementChild);
