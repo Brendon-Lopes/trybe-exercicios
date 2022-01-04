c3fda3b47cd4077f7bd1ef188ec1df3211e593ee
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-01-04 20:32:51 -0300

    estados criados dinamicamente

---
+++ 01-fundamentos/bloco-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-01-HTML-e-CSS-Forms/exercicio-03/script.js
@@ -0,0 +1,38 @@
+const stateInput = document.getElementById('state-input');
+
+const states = [
+  { 'AC': 'Acre' },
+  { 'AL': 'Alagoas' },
+  { 'AP': 'Amapá' },
+  { 'AM': 'Amazonas' },
+  { 'BA': 'Bahia' },
+  { 'CE': 'Ceará' },
+  { 'DF': 'Distrito Federal' },
+  { 'ES': 'Espírito Santo' },
+  { 'GO': 'Goías' },
+  { 'MA': 'Maranhão' },
+  { 'MT': 'Mato Grosso' },
+  { 'MS': 'Mato Grosso do Sul' },
+  { 'MG': 'Minas Gerais' },
+  { 'PA': 'Pará' },
+  { 'PB': 'Paraíba' },
+  { 'PR': 'Paraná' },
+  { 'PE': 'Pernambuco' },
+  { 'PI': 'Piauí' },
+  { 'RJ': 'Rio de Janeiro' },
+  { 'RN': 'Rio Grande do Norte' },
+  { 'RS': 'Rio Grande do Sul' },
+  { 'RO': 'Rondônia' },
+  { 'RR': 'Roraíma' },
+  { 'SC': 'Santa Catarina' },
+  { 'SP': 'São Paulo' },
+  { 'SE': 'Sergipe' },
+  { 'TO': 'Tocantins' },
+];
+
+for(const i of states) {
+  const createOption = document.createElement('option');
+  createOption.innerText = Object.values(i);
+  createOption.setAttribute('value', Object.keys(i));
+  stateInput.appendChild(createOption);
+}
