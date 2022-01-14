c9df7085ef042c288d45ea122dd5bf842efee53c
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-01-14 19:20:09 -0300

    Exercicio 1 concluido

--- 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
+++ 01-fundamentos/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-03-primeiros-passos-em-jest/sum.test.js
@@ -3,3 +3,19 @@ const sum = require('./sum');
 test('sum 4 + 5 returns 9', () => {
   expect(sum(4, 5)).toBe(9);
 });
+
+test('sum 0 + 0 returns 0', () => {
+  expect(sum(0, 0)).toBe(0);
+});
+
+test('sum 4 + "5" returns error', () => {
+  expect(() => {
+      sum(0, '5');
+    }).toThrow();
+});
+
+test('sum 4 + "5" returns "parameters must be numbers"', () => {
+  expect(() => {
+    sum(0, '5');
+  }).toThrow('parameters must be numbers');
+});
