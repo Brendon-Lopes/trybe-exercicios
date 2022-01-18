d680eb6a290c0fe088f2488e771c32d52365a6b7
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-01-18 19:02:27 -0300

    Exercicio 3 concluido

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/exercicio3.js
@@ -0,0 +1,19 @@
+const RIGHT_ANSWERS = ["A", "C", "B", "D", "A", "A", "D", "A", "D", "C"];
+const STUDENT_ANSWERS = ["A", "N.A", "B", "D", "A", "C", "N.A", "A", "D", "B"];
+
+const answersChecker = (rightAnswers, studentAnswers) => {
+  if(rightAnswers === studentAnswers) return 1;
+  if(studentAnswers === 'N.A') return 0;
+  return -0.5;
+}
+
+const gradeCalculator = (rightAnswers, studentAnswers, callback) => {
+  let grade = 0;
+
+  for(let index = 0; index < rightAnswers.length; index += 1) {
+    grade += callback(rightAnswers[index], studentAnswers[index]);
+  }
+  return grade;
+}
+
+console.log(gradeCalculator(RIGHT_ANSWERS, STUDENT_ANSWERS, answersChecker));
