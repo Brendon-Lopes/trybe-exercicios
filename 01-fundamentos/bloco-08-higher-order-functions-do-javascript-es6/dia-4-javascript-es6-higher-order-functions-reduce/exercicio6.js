4f87a568d40a840b51aa298333aecb57ea77165e
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-01-22 13:37:00 -0300

    Exercicio 6 concluido

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercicio6.js
@@ -0,0 +1,46 @@
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+function calcAverage(index) {
+  return (grades[index]
+    .reduce((acc, grade) => acc + grade, 0)) / grades[index].length;
+}
+
+function studentsAverage() {
+  return students.map((student, index) => ({
+    name: student,
+    average: calcAverage(index),
+  }))
+}
+
+console.log(studentsAverage());
+
+
+// function studentAverage() {
+//   return [
+//     {
+//       name: students[0],
+//       average: parseFloat(grades[0]
+//         .reduce((acc, curr, index, array) => acc + curr / array.length, 0)
+//         .toFixed(1))
+//     },
+//     {
+//       name: students[1],
+//       average: parseFloat(grades[1]
+//         .reduce((acc, curr, index, array) => acc + curr / array.length, 0)
+//         .toFixed(1))
+//     },
+//     {
+//       name: students[2],
+//       average: parseFloat(grades[2]
+//         .reduce((acc, curr, index, array) => acc + curr / array.length, 0)
+//         .toFixed(1))
+//     }
+//   ]
+// }
