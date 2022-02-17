c906c0e94ff2050902061f53311a5edf5cfe37fe
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-02-17 20:11:10 -0300

    fixed PropTypes check

--- 02-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/2-fixation-exercises-10-2/src/UserProfile.jsx
+++ 02-front-end/bloco-10-introducao-a-react/dia-2-componentes-react/2-fixation-exercises-10-2/src/UserProfile.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import Image from "./Image";
+import PropTypes from 'prop-types';
 
 class UserProfile extends React.Component {
   render() {
+    const { name, email, avatar } = this.props.user;
+
     return (
       <div>
-        <Image source={this.props.user.avatar} alternativeText={this.props.user.name}/>
-        <p>Name: {this.props.user.name}</p>
-        <p>Email: {this.props.user.email}</p>
+        <Image source={ avatar } alternativeText={ name }/>
+        <p>Name: { name }</p>
+        <p>Email: { email }</p>
       </div>
     )
   }
+}
+
+UserProfile.propTypes = {
+  user: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+  })
 }
 
 export default UserProfile;
