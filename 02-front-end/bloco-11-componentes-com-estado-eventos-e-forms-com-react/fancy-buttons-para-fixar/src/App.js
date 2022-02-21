73b209c1aa42b30fecfa6e3a22d8fc921d152e34
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-02-21 15:40:46 -0300

    Added constructor to fixation exercise

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-forms-com-react/fancy-buttons-para-fixar/src/App.js
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-forms-com-react/fancy-buttons-para-fixar/src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './App.css';
 
-const botao1 = () => console.log('Botão 1');
-const botao2 = () => console.log('Botão 2');
-const botao3 = () => console.log('Botão 3');
-
-
 class App extends React.Component {
+  constructor() {
+    super();
+    this.botao1 = this.botao1.bind(this);
+  }
+
+  botao1() { console.log(this, 'Botão 1'); }; // função comum precisa do bind para exibir 'this'.
+  botao2 = () => console.log(this, 'Botão 2'); // arrow function não precisa de bind, mas é menos performática.
+  botao3 = () => console.log(this, 'Botão 3');
+
   render() {
     return (
       <div>
-        <button onClick={botao1}>Botão 1</button>
-        <button onClick={botao2}>Botão 2</button>
-        <button onClick={botao3}>Botão 3</button>
+        <button onClick={this.botao1}>Botão 1</button>
+        <button onClick={this.botao2}>Botão 2</button>
+        <button onClick={this.botao3}>Botão 3</button>
       </div>
     )
   }
