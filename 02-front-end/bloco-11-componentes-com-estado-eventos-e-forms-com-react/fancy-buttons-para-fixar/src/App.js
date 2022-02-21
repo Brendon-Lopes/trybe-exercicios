9bf017b4159a21e6717ffd59865c4ff45732759b
Brendon Lopes <brendon.lopes.21@gmail.com>
2022-02-21 16:14:36 -0300

    Added new functions to fixation exercise

--- 02-front-end/bloco-11-componentes-com-estado-eventos-e-forms-com-react/fancy-buttons-para-fixar/src/App.js
+++ 02-front-end/bloco-11-componentes-com-estado-eventos-e-forms-com-react/fancy-buttons-para-fixar/src/App.js
@@ -5,18 +5,34 @@ class App extends React.Component {
   constructor() {
     super();
     this.botao1 = this.botao1.bind(this);
+    this.state = {
+      numCliques: 0
+    }
   }
 
-  botao1() { console.log(this, 'Botão 1'); }; // função comum precisa do bind para exibir 'this'.
+  botao1() { // função comum precisa do bind para exibir 'this'.
+    this.setState((prevState, _props) => ({
+      numCliques: prevState.numCliques + 1
+    }));
+    if(this.state.numCliques % 2 === 0) {
+
+    }
+  };
   botao2 = () => console.log(this, 'Botão 2'); // arrow function não precisa de bind, mas é menos performática.
-  botao3 = () => console.log(this, 'Botão 3');
 
   render() {
     return (
       <div>
-        <button onClick={this.botao1}>Botão 1</button>
+        <button
+        onClick={this.botao1}
+        style={{backgroundColor: this.state.numCliques % 2 === 0 ? 'green' : 'initial'}}
+        >{this.state.numCliques}
+        </button>
+
+        {/* <button onClick={ () => this.botao1('parâmetros') }>{this.state.numCliques}</button>
+        ^ forma de chamar uma função com parâmetros no evento onClick */ }
+        
         <button onClick={this.botao2}>Botão 2</button>
-        <button onClick={this.botao3}>Botão 3</button>
       </div>
     )
   }
